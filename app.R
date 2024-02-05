c261f75363f17927465e14528d06522e486c0fe7
Ji Huang <timedreamer@users.noreply.github.com>
2024-02-05 13:18:22 -0500

    init commit.

---
+++ app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(here)
+library(DT)
+
+# Assuming your data is in a DataFrame called 'gene_data'
+# Read your data here, e.g., gene_data <- read.csv("your_data.csv")
+
+# data <- read_tsv(here("data", "orthology", "zma__v__ath.tsv"))
+# saveRDS(data, here("data", "orthology", "zma__v__ath.RDS"))
+
+data <- readRDS(here("zma__v__ath.RDS"))
+
+
+ui <- fluidPage(
+    titlePanel("Gene ID Search in Orthogroups"),
+    sidebarLayout(
+        sidebarPanel(
+            textInput("gene_id", "Enter Gene IDs (separated by commas)"),
+            actionButton("search", "Search")
+        ),
+        mainPanel(
+            DTOutput("results")
+        )
+    )
+)
+
+server <- function(input, output) {
+    result <- eventReactive(input$search, {
+        req(input$gene_id)
+        searchTerms <- strsplit(input$gene_id, ",\\s*")[[1]]
+        matches <- sapply(searchTerms, function(term) {
+            grepl(term, data$zma) | grepl(term, data$ath)
+        })
+        data[rowSums(matches) > 0, ]
+    })
+    
+    output$results <- renderDT({
+        result()
+    }, options = list(pageLength = 5))
+}
+
+shinyApp(ui, server)
